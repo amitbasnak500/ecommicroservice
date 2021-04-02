1faeca695be4289f886dbad8286b1502a82a5ddd
amitbasnak500 <79815917+amitbasnak500@users.noreply.github.com>
2021-04-01 21:36:47 -0500

    Create OrderBatchJobConstant.java

---
+++ ecommerce-bulk-order-service/src/main/java/com/ecommerce/bulk/order/constants/OrderBatchJobConstant.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.ecommerce.bulk.order.constants;
+
+/**
+ * @author amit.basnak
+ *
+ */
+public interface OrderBatchJobConstant
+{
+	String ORDER_ITEM_READER ="orderItemReader";
+    String ORDER_PROCESS_JOB ="orderProcessJob";
+
+    String BATCH_STEP="step";
+}
