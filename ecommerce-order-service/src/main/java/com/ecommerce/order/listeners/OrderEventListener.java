5314125b59fe13a7e076442e2d40e56d797236b2
amitbasnak500 <79815917+amitbasnak500@users.noreply.github.com>
2021-04-01 20:55:46 -0500

    Create OrderEventListener.java

---
+++ ecommerce-order-service/src/main/java/com/ecommerce/order/listeners/OrderEventListener.java
@@ -0,0 +1,21 @@
+package com.ecommerce.order.listeners;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.ecommerce.order.events.OrderEvent;
+
+@Component
+public class OrderEventListener 
+{
+
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	@EventListener
+	public void onApplicationEvent(OrderEvent orderEvent) {
+		log.info("Received Order Event : "+orderEvent.getEventType());
+		log.info("Received Order From Order Event :"+orderEvent.getOrder().toString());
+	}
+}
