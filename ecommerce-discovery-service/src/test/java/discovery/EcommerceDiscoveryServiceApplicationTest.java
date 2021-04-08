4cc415570a6b6f89308c3573eaa700b0e318affa
amitbasnak500 <79815917+amitbasnak500@users.noreply.github.com>
2021-04-08 00:03:47 -0500

    Create EcommerceDiscoveryServiceApplicationTest.java

---
+++ ecommerce-discovery-service/src/test/java/discovery/EcommerceDiscoveryServiceApplicationTest.java
@@ -0,0 +1,27 @@
+package discovery;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class EcommerceDiscoveryServiceApplicationTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	@Test
+	public void contextLoads() {
+	}
+
+
+}
